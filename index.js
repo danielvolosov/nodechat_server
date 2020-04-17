1cf5d52d4a27967b5d6fcc85cdbb81545f4de2c9
Daniel <danielvolosov@outlook.com>
2020-04-17 18:34:04 -0400

    user service

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users/users');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -12,6 +14,16 @@ const io = socketio(server);
 
 io.on('connection', (socket) => {
  console.log('We have a new connection');   
+
+ socket.on('join', ({ name, room }, callback) => {
+    console.log(name, room);
+
+    const error = true;
+
+    if(error) {
+        callback({ error: 'error' });
+    }
+ })
 
  socket.on('disconnect', () => {
      console.log('User has left');
